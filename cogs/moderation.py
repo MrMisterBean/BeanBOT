19ea8338cfc2f43157119053f52430cbf2d9c2c1
MrMisterBean <alutter03@gmail.com>
2021-02-18 12:32:21 -0500

    System for unloading and loading cogs

---
+++ cogs/moderation.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+
+
+class Moderation(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+    # clears specified number of messages
+    @commands.command()
+    async def clear(self, ctx, amount=10):
+        await ctx.channel.purge(limit = amount)
+
+    # kicks a user
+    @commands.command()
+    async def kick(self, ctx, member : discord.Member, *, reason = None): 
+        await member.kick(reason = reason)
+        await ctx.send(f'Kicked {member.mention}\nReason: {reason}')
+        
+    # bans a user
+    @commands.command()
+    async def ban(self, ctx, member : discord.Member, *, reason = None): 
+        await member.ban(reason = reason)
+        await ctx.send(f'Banned {member.mention}\nReason: {reason}')
+
+    # unbans a user
+    @commands.command()
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.member_discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned {user.mention}')
+                return
+            else: print("ERROR. User not found.")
+
+
+def setup(client):
+    client.add_cog(Moderation(client))
---
+++ core.py
@@ -0,0 +1,32 @@
+import discord 
+import os
+from discord.ext import commands
+
+
+
+#creates variable client to avoind having to type this crap 1,000 times
+client = commands.Bot(command_prefix = '.')
+# function to load an extension (cog)
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+# function to unload an extension (cog)
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+# load each .py file in the cogs folder 
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[: -3]}')
+
+# print when it is ready to go
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+
+# returns bot latency (ping) in milliseconds 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+client.run('ODExNzU3NjYwNzM5MjA3MjQw.YC22PA.1JXnyc1dRRyL2cZmed39x2E3B-8')
